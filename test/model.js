c0f499d4f3ff06875571d24dbddeb64a7d9721ea
Benjamin Pannell <admin@sierrasoftworks.com>
2013-12-03 03:06:21 +0200

    Added more tests for Model, focusing on DB CRUD

--- test/model.js
+++ test/model.js
@@ -29,5 +29,88 @@ describe('orm', function () {
 				});
 			});
 		});
+
+		describe('database', function() {
+			var model = null;
+
+			before(function(done) {
+				model =  new Model(db, 'model', {
+					name: String
+				}, {
+					renames: {
+						_id: 'name'
+					},
+					transforms: {
+						_id: false
+					}
+				});
+
+				model.remove(done);
+			});
+
+			describe('insertion', function() {
+
+				it('should allow a single object to be inserted', function(done) {
+					model.create({
+						name: 'Demo1'
+					}, function(err, instance) {
+						if(err) return done(err);
+						instance.should.have.ownProperty('name', 'Demo1');
+						return done();
+					});
+				});
+
+				it('should have created the instance in the database', function(done) {
+					model.count({ name: 'Demo1' }, function(err, number) {
+						if(err) return done(err);
+						number.should.eql(1);
+						done();
+					});
+				});
+
+				it('should allow multiple objects to be inserted', function(done) {
+					model.create([{
+						name: 'Demo2'
+					}, {
+						name: 'Demo3'
+					}], function(err, instances) {
+						if(err) return done(err);
+						should(Array.isArray(instances));
+						instances[0].should.have.ownProperty('name', 'Demo2');
+						instances[1].should.have.ownProperty('name', 'Demo3');
+						return done();
+					});
+				});
+
+				it('should pass along DB errors', function(done) {
+					model.create({
+						name: 'Demo1'
+					}, function(err, inserted) {
+						should.exist(err);
+						should.not.exist(inserted);
+						done();
+					});
+				});
+			});
+
+			describe('findOne', function() {
+				it('should be able to locate a single object', function(done) {
+					model.findOne({ name: 'Demo1' }, function(err, obj) {
+						if(err) return done(err);
+						should.exist(obj);
+						obj.should.have.ownProperty('name', 'Demo1');
+						done();
+					})
+				});
+
+				it('should not throw an error if it couldn\'t find an object', function(done) {
+					model.findOne({ name: 'NotFound' }, function(err, obj) {
+						if(err) return done(err);
+						should.not.exist(obj);
+						done();
+					})
+				});
+			});
+		});
 	});
 });
