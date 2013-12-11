fa38476d0317c18756cc21d347fb3c9c03b6af9e
Benjamin Pannell <admin@sierrasoftworks.com>
2013-12-11 12:50:38 +0200

    Added more plugin tests

--- test/plugins.js
+++ test/plugins.js
@@ -31,4 +31,44 @@ describe('plugins', function() {
 			});
 		});
 	});
+
+	describe('model creation', function() {
+		var plugin = {
+			newModel: function(db, collection, schema, options) {
+				options.done();
+			}
+		};
+
+		it('should correctly trigger the newModel hook', function(done) {
+			var model = new Model({
+					plugins: [plugin],
+					db: {
+						collection: function() { return null; }
+					}
+				}, 'collection',
+				{ name: String, age: 'Positive'	},
+				{ done: done });
+		});
+	});
+
+	describe('instance creation', function() {
+		var plugin = {
+			newInstance: function(model, doc, isNew) {
+				model.options.done();
+			}
+		};
+
+		it('should correctly trigger the newInstance hook', function(done) {
+			var model = new Model({
+					plugins: [plugin]
+				}, 'collection',
+				{ name: String, age: 'Positive'	},
+				{ done: done, preprocessors: [] });
+
+			var instance = new Instance(model, {
+				name: 'Test',
+				age: 10
+			});
+		});
+	});
 });
