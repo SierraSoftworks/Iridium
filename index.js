378799b2ed312c3a1a715f23a5927b552749c846
Benjamin Pannell <admin@sierrasoftworks.com>
2013-12-13 05:37:58 +0200

    Added global Concoction export

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 ﻿/// <reference path="nodelib/node.js"/>
 /// <reference path="lib/Database.js"/>
 var Database = require('./lib/Database');
+var Concoction = require('concoction');
+
+Database.Concoction = Concoction;
 
 (require.modules || {}).iridium = (require.modules || {}).index = module.exports = Database;
