ab9eb07928a3761a2c486f8112cce970b0d68d90
Benjamin Pannell <admin@sierrasoftworks.com>
2014-01-15 00:19:47 +0200

    Updated Diff engine We've moved to a new diff engine (codename Omnom) which is so much better than the original diff algorithm as to make any comparison laughable.
    The biggest reason for the move was to allow us to support array diffs
    in an intelligent way while still allowing single queries to function
    correctly.
    
    To achieve this, there are a few important implications which you need
    to take into account - namely that using the built in algorithm for combinations
    of pushes and pulls on arrays is a TERRIBLE IDEA. This is due to the way
    that arrays are handled to support single queries - since MongoDB doesn't
    allow combinations of $push/$pull/$set on an array within a single query
    for consistency reasons.
    
    USE IT FOR:
     - Changing the values of array elements (and their child values) without
       changing their position in the array
     - Adding array elements and making changes to existing ones (at the same time if you wish)
    
    BIG DON'TS:
     - Removing array elements while adding/modifying others
     - Insertion at the front of an array (Forces the array to be replaced)

---
+++ test/instance_db.js
@@ -0,0 +1,19 @@
+var Database = require('../index');
+var Model = Database.Model;
+var Instance = Database.Instance;
+var should = require('should');
+var Concoction = require('concoction');
+
+describe('orm', function () {
+	"use strict";
+
+	describe('Instance', function () {
+		var db = {
+			plugins: []
+		};
+
+		describe('database', function () {
+			
+		});
+	});
+});
