edf42c32b8c56fadc745ba273b97acb1d3f9534e
Benjamin Pannell <admin@sierrasoftworks.com>
2014-01-15 12:44:14 +0200

    Updates to Instance  - Fixes an error when saving instances without custom change sets  - Adds a `document` virtual property which returns the backing document from the database  - Adds a `select` function which allows you to easily select array/object values based on    a selector function (makes working with arrays containing documents easier)  - Adds a `first` function which does the same as select, but only returns the first result.

--- test/instance_db.js
+++ test/instance_db.js
@@ -8,12 +8,58 @@ describe('orm', function () {
 	"use strict";
 
 	describe('Instance', function () {
-		var db = {
-			plugins: []
-		};
+		var db = new Database({ database: 'iridium_test' });
+
+		var model = new Model(db, 'instances', {
+			username: String,
+			sessions: [{
+				id: String,
+				expires: Date
+			}]
+		}, {
+			preprocessors: [
+				new Concoction.Rename({ '_id': 'username' })
+			]
+		});
+
+		before(function(done) {
+			db.connect(function(err) {
+				if(err) return done(err);
+				model.remove(done);
+			});
+		});
+
+		after(function() {
+			db.disconnect();
+		});
 
 		describe('database', function () {
-			
+			describe('save', function() {
+				it('should correctly store changes made to the instance', function(done) {
+					model.create({
+						username: 'billy',
+						sessions: [{
+							id: 'aaaa',
+							expires: new Date()
+						}]
+					}, function(err, original) {
+						if(err) return done(err);
+
+						original.sessions.push({
+							id: 'bbbb',
+							expires: new Date()
+						});
+
+						original.save(function(err, updated) {
+							if(err) return done(err);
+							updated.should.equal(original);
+							updated.sessions.length.toString().should.eql('2');
+
+							done();
+						});
+					});
+				});
+			});
 		});
 	});
 });
