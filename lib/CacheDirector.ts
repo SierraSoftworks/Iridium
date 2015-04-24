6379d10daa8c59aa1cda3946ebdec8d40479481f
Benjamin Pannell <admin@sierrasoftworks.com>
2015-04-24 16:33:51 +0200

    Changed the way cache controllers are implemented

--- lib/CacheDirector.ts
+++ lib/CacheDirector.ts
@@ -4,4 +4,7 @@ export = ICacheDirector;
 interface ICacheDirector {
     valid<T>(object: T): boolean;
     buildKey<T>(object: T): string;
+
+    validQuery(conditions: any): boolean;
+    buildQueryKey(conditions: any): string;
 }
