252ee7734734c18e859e289f26b00dd4e60b6887
Benjamin Pannell <admin@sierrasoftworks.com>
2015-04-24 16:34:06 +0200

    Removed the beforeSave hook (since it is handled by saving)

--- lib/Hooks.ts
+++ lib/Hooks.ts
@@ -5,6 +5,5 @@ export interface IHooks<TDocument, TInstance> {
     creating? (document: TDocument);
     retrieved? (document: TDocument);
     ready? (instance: TInstance);
-    beforeSave? (instance: TInstance);
     saving? (instance: TInstance, changes: any);
 }
