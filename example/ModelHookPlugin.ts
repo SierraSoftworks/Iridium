acd8be693a23274cfb71070a0df30ae4873dee4d
Benjamin Pannell <admin@sierrasoftworks.com>
2015-04-29 21:46:16 +0200

    Updated examples to use the module exports rather than internal modules

--- example/ModelHookPlugin.ts
+++ example/ModelHookPlugin.ts
@@ -1,11 +1,9 @@
 import Iridium = require('../index');
-import Model = require('../lib/Model');
-import IPlugin = require("../lib/Plugins");
 
 export = LowercaseCollectionsPlugin;
 
-class LowercaseCollectionsPlugin implements IPlugin {
-    newModel(model: Model.IModel<any, any>) {
+class LowercaseCollectionsPlugin implements Iridium.Plugin {
+    newModel(model: Iridium.Model<any, any>) {
         model.collectionName = model.collectionName.toLowerCase();
     }
 }
--- example/ValidationPlugin.ts
+++ example/ValidationPlugin.ts
@@ -1,10 +1,10 @@
 /// <reference path="../typings/skmatc/skmatc.d.ts"/>
-import Plugin = require('../lib/Plugins');
+import Iridium = require('../index');
 import Skmatc = require('skmatc');
 
 export = StringCaseValidationPlugin;
 
-class StringCaseValidationPlugin implements Plugin {
+class StringCaseValidationPlugin implements Iridium.Plugin {
     validate = [
         Skmatc.create((schema) => schema == "Lowercase", function (schema, data, path) { return this.assert(data.toLowerCase() == data) }),
         Skmatc.create((schema) => schema == "Uppercase", function (schema, data, path) { return this.assert(data.toUpperCase() == data) })
