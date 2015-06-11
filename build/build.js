d30809e8162aa11ec1fa64e504916c373fe62b21
Benjamin Pannell <admin@sierrasoftworks.com>
2015-06-11 10:25:22 +0200

    Some updates to gulp tasks

--- build/build.js
+++ build/build.js
@@ -1,7 +1,6 @@
 var gulp       = require('gulp'),
 	typescript = require('gulp-typescript'),
 	sourcemaps = require('gulp-sourcemaps'),
-	plumber    = require('gulp-plumber'),
 	changed    = require('gulp-changed');
 	
 var paths = require('./paths');
@@ -16,7 +15,6 @@ gulp.task('build', ['build-lib', 'build-tests']);
 
 gulp.task('build-lib', function () {
 	var tsResult = gulp.src(paths.buildFiles, { base: paths.projectRoot })
-		.pipe(plumber())
 		.pipe(sourcemaps.init())
 		.pipe(typescript(tsProject));
 		
@@ -27,7 +25,6 @@ gulp.task('build-lib', function () {
 
 gulp.task('build-tests', function () {
 	var tsResult = gulp.src(paths.testFiles, { base: paths.projectRoot })
-		.pipe(plumber())
 		.pipe(sourcemaps.init())
 		.pipe(typescript(tsProject));
 		
--- build/test.js
+++ build/test.js
@@ -7,7 +7,6 @@ var paths = require('./paths');
 
 gulp.task('test', function () {
 	return gulp.src(paths.builtTestFiles)
-		.pipe(plumber())
 		.pipe(mocha({
 			require: paths.testSupportFiles.map(function (file) { return path.resolve(path.dirname(__dirname), file); }),
 			timeout: 10000
