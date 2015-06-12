6bbf45b61f93873d8b9320c7b351872878e83cf7
Benjamin Pannell <admin@sierrasoftworks.com>
2015-06-12 17:27:23 +0200

    Removed un-needed dependency

--- _references.d.ts
+++ _references.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="./typings/DefinitelyTyped/tsd.d.ts" />
 /// <reference path="./typings/skmatc/skmatc.d.ts" />
-/// <reference path="./node_modules/reflect-metadata/reflect-metadata.d.ts" />
