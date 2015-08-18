b9c13fbcc8f521cf094f469b7f86be5928e4037e
Benjamin Pannell <admin@sierrasoftworks.com>
2015-08-18 16:27:13 +0200

    Updated changelog generation

--- build/changelog.js
+++ build/changelog.js
@@ -3,9 +3,9 @@ var gulp = require('gulp'),
 	git = require('gulp-git'),
 	replace = require('gulp-replace'),
 	path = require('path');
-	
+
 var repo = require('../package.json').repository.url;
-	
+
 gulp.task('changelog', function(done) {
 	git.exec({ args: 'log --oneline --decorate' }, function(err, stdout) {
 		if(err) return done(err);
@@ -13,14 +13,14 @@ gulp.task('changelog', function(done) {
 			.pipe(replace(/.|\n/g, ''))
 			.pipe(replace(/$/, stdout))
 			.pipe(replace(/#(\d+)/, '[#$1](' + repo + '/issues/$1)'))
-			.pipe(replace(/([a-f0-9]{7}) \(HEAD[^)]*\) (.+)/g, function(_, sha, comment) {
-				return '\n## [Working Changes](' + repo + ')' +
-					'\n- [' + sha + '](' + repo + '/commit/' + sha + ') ' + comment;
-			}))
-			.pipe(replace(/([a-f0-9]{7}) \(tag: ([^\s),]*)[^)]*\) (.+)/g, function(_, sha, tag, comment) {
+			.pipe(replace(/([a-f0-9]{7}) \((?:HEAD, )?tag: ([^\s),]*)[^)]*\) (.+)/g, function(_, sha, tag, comment) {
 				var niceTag = tag;
 				if(tag.indexOf('v') !== 0) niceTag = 'v' + tag;
 				return '\n## [' + niceTag + '](' + repo + '/tree/' + tag + ')' +
+					'\n- [' + sha + '](' + repo + '/commit/' + sha + ') ' + comment;
+			}))
+			.pipe(replace(/([a-f0-9]{7}) \(HEAD[^)]*\) (.+)/g, function(_, sha, comment) {
+				return '\n## [Working Changes](' + repo + ')' +
 					'\n- [' + sha + '](' + repo + '/commit/' + sha + ') ' + comment;
 			}))
 			.pipe(replace(/([a-f0-9]{7}) (.+)/g, '- [$1](' + repo + '/commit/$1) $2'))
