711c490fe2bdcf33484ce55827fc0ca281dea55a
Benjamin Pannell <admin@sierrasoftworks.com>
2015-08-30 21:08:38 +0200

    Added gulp script to automatically update documentation

--- build/doc.js
+++ build/doc.js
@@ -1,9 +1,39 @@
 var gulp = require('gulp'),
-	typedoc = require('gulp-typedoc');
+	git = require('gulp-git'),
+	typedoc = require('gulp-typedoc'),
+	path = require('path'),
+	runSequence = require('run-sequence'),
+	fs = require('fs');
 	
 var paths = require('./paths');
 	
 gulp.task('doc', function() {
+	return runSequence('doc-compile', 'doc-submodule', 'doc-commit');
+});
+	
+gulp.task('doc-submodule', function(cb) {
+	fs.writeFile('doc/.git', 'gitdir: ../.git/modules/doc', cb);
+});
+
+gulp.task('doc-commit', function(cb) {
+	runSequence('doc-commit-changes', 'doc-commit-newdocs', cb);
+});
+
+gulp.task('doc-commit-changes', function() {
+	return gulp.src('doc')
+		.pipe(git.commit('Updated documentation'));
+});
+	
+gulp.task('doc-commit-newdocs', function() {
+	return gulp.src('.')
+		.pipe(git.commit('Updated documentation'));
+});
+	
+gulp.task('doc-push', function(cb) {
+	git.push('origin', 'gh-pages', { cwd: 'doc' }, cb);
+});
+	
+gulp.task('doc-compile', function() {
 	return gulp.src(paths.docSourceFiles).pipe(typedoc({
 		module: 'commonjs',
 		target: 'es5',
@@ -14,7 +44,6 @@ gulp.task('doc', function() {
 		
 		name: "Iridium",
 		plugin: ['decorator'],
-		ignoreCompilerErrors: true,
-		version: true
+		ignoreCompilerErrors: false
 	}));
 });
