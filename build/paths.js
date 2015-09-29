a8f4c016c4aac2a34528e4b4c07b059f6bfaa892
Benjamin Pannell <admin@sierrasoftworks.com>
2015-09-29 11:14:29 +0200

    Version 6.0.0-alpha.1

--- build/paths.js
+++ build/paths.js
@@ -3,22 +3,23 @@ module.exports = {
 	distFolder: path.resolve(__dirname, '../dist'),
 	projectRoot: path.dirname(__dirname),
 	coverageFolder: path.resolve(__dirname, '../coverage'),
-	
+
 	buildFiles: ["lib/**/*.ts", "index.ts"],
 	testFiles: ["test/**/*.ts"],
 	cleanFiles: ["coverage", "dist"],
-	
+
 	builtTestFiles: 'dist/test/*.js',
 	builtFiles: ["dist/lib/**/*.js", "dist/index.js"],
 	testSupportFiles: ['dist/test/support/chai'],
-	
+
 	docSourceFiles: [
+		"_references.d.ts",
 		"lib/Core.ts",
 		"lib/Model.ts",
 		"lib/Instance.ts",
-		
+
 		"lib/Decorators.ts",
-		
+
 		"lib/Plugins.ts",
 		"lib/Schema.ts",
 		"lib/Cache.ts",
@@ -27,12 +28,12 @@ module.exports = {
 		"lib/Configuration.ts",
 		"lib/Hooks.ts",
 		"lib/Transforms.ts",
-		
+
 		"lib/caches/MemoryCache.ts",
 		"lib/caches/NoOpCache.ts",
-		
+
 		"lib/cacheControllers/IDDirector.ts",
-		
+
 		"lib/utils/ObjectID.ts"
 	]
 };
