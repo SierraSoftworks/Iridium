0db5f349ab09b3ccaa6c9fc1a0e14910a1a39308
Louy <louy08@gmail.com>
2015-11-16 10:21:19 +0200

    Promises support in hooks

--- lib/Hooks.ts
+++ lib/Hooks.ts
@@ -1,8 +1,9 @@
 import instance = require('./Instance');
+import Bluebird = require('bluebird');
 
 export interface Hooks<TDocument, TInstance> {
-    onCreating? (document: TDocument): void;
-    onRetrieved? (document: TDocument): void;
-    onReady? (instance: TInstance): void;
-    onSaving? (instance: TInstance, changes: any): void;
+    onCreating? (document: TDocument): Promise.Thenable<void> | void;
+    onRetrieved? (document: TDocument): Promise.Thenable<void> | void;
+    onReady? (instance: TInstance): Promise.Thenable<void> | void;
+    onSaving? (instance: TInstance, changes: any): Promise.Thenable<void> | void;
 }
