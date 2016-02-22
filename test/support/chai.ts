8d3e3118f41135450884f9b6c832a386ea72fa9b
Benjamin Pannell <admin@sierrasoftworks.com>
2016-02-22 13:57:28 +0200

    fix: Update various libraries to their latest versions

--- test/support/chai.ts
+++ test/support/chai.ts
@@ -2,12 +2,10 @@
 import chai = require('chai');
 import chaiAsPromised = require('chai-as-promised');
 import Bluebird = require('bluebird');
-var chaiFuzzy = require('chai-fuzzy');
 
 Bluebird.longStackTraces();
 
 chai.use(chaiAsPromised);
-chai.use(chaiFuzzy);
 
 global.chai = chai;
 global.expect = chai.expect;
