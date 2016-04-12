6bc3587c37b735366e7b6be99d33383e2af99bc3
Benjamin Pannell <admin@sierrasoftworks.com>
2016-04-12 15:49:47 +0200

    refactor: Switch to promises-a-plus for cache promise definition

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="main/ambient/es6-promise/index.d.ts" />
 /// <reference path="main/ambient/mocha/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
+/// <reference path="main/ambient/promises-a-plus/index.d.ts" />
 /// <reference path="main/definitions/bluebird/index.d.ts" />
 /// <reference path="main/definitions/chai-as-promised/index.d.ts" />
 /// <reference path="main/definitions/chai/index.d.ts" />
