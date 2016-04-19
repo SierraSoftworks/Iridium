6de82890837db17a781d9d6c33ed4d000b94e42d
Benjamin Pannell <admin@sierrasoftworks.com>
2016-04-19 18:49:52 +0200

    tweak: Point doc generation at iridium index

--- build/paths.js
+++ build/paths.js
@@ -12,27 +12,7 @@ module.exports = {
 
 	docSourceFiles: [
 		"typings/main.d.ts",
-		"lib/Core.ts",
-		"lib/Model.ts",
-		"lib/Instance.ts",
-
-		"lib/Decorators.ts",
-
-		"lib/Plugins.ts",
-		"lib/Schema.ts",
-		"lib/Cache.ts",
-		"lib/CacheDirector.ts",
-		"lib/ModelOptions.ts",
-		"lib/Configuration.ts",
-		"lib/Hooks.ts",
-		"lib/Transforms.ts",
-
-		"lib/caches/MemoryCache.ts",
-		"lib/caches/NoOpCache.ts",
-
-		"lib/cacheControllers/IDDirector.ts",
-
-		"lib/utils/ObjectID.ts"
+		"iridium.ts"
 	],
 	testSupportFiles: ['dist/test/support/chai']
 };
