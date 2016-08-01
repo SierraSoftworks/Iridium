778eaf746f0313b2c48b7798fc6c75ad5aaa908a
Benjamin Pannell <admin@sierrasoftworks.com>
2016-08-01 17:36:44 +0200

    test: Add sourcemap support to test stack traces

--- test/support/chai.ts
+++ test/support/chai.ts
@@ -1,7 +1,11 @@
 import * as chai from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import * as Bluebird from "bluebird";
+import * as sourceMapSupport from "source-map-support";
 
 Bluebird.longStackTraces();
+sourceMapSupport.install({
+    handleUncaughtExceptions: false
+});
 
 chai.use(chaiAsPromised);
