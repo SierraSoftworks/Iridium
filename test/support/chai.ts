00cf41e2c787c5e6b739690a98bdb10278fa1f07
Benjamin Pannell <admin@sierrasoftworks.com>
2018-01-16 18:01:38 +0000

    test: Use Bluebird as the Promise implementation for testing

--- test/support/chai.ts
+++ test/support/chai.ts
@@ -1,6 +1,13 @@
+import * as Bluebird from "bluebird";
 import * as chai from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import * as sourceMapSupport from "source-map-support";
+
+Bluebird.config({
+    longStackTraces: true
+})
+
+global.Promise = Bluebird;
 
 sourceMapSupport.install({
     handleUncaughtExceptions: false
